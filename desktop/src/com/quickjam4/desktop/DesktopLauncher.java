57526e8e6599e1d70fb958cba65f8f74307b2e6c
Declan Whitford Jones <wjdeclan@gmail.com>
2016-12-17 14:36:11 -0500

    Initial libgdx build

---
+++ desktop/src/com/quickjam4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quickjam4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quickjam4.QuickJam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QuickJam(), config);
+	}
+}
